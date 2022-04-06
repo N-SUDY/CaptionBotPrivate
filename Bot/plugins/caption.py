84f40e1ac8e109a6d6ff5cc9d238899a1c943eaa
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-06 22:01:39 +0430

    Update caption.py

--- Bot/plugins/caption.py
+++ Bot/plugins/caption.py
@@ -1,35 +1,16 @@
-import asyncio,os
+from pyromod import listen  
 
-from pyrogram.types.messages_and_media import message
-from pyromod import listen 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 from Bot import app
 
 @app.on_message(filters.private & filters.media & ~filters.edited, group=4)
 async def incoming(c: Client, m: Message):
-    name=None
-    if m.photo:
-      file=m.photo.file_id
-      type_="pic"
-    elif m.video:
-      file=m.video.file_id
-      type_="video"
-    elif m.document:
-      file=m.document.file_id
-      type_="document"
-    elif m.audio:
-      file=m.audio.file_id
-      type_="audio"
-    else:
-      return await m.reply("فرمت فایل پشتیبانی نمی شود")
     caption = await get_caption(c, m)
     if caption is True:
         return
     await m.copy(chat_id=m.chat.id, caption=caption, reply_to_message_id=m.message_id)
-    await c.edit_message_caption(chat_id=m.chat.id, message_id=m.message_id, caption=caption)
 
 async def get_caption(c: Client, m: Message):
     caption = await c.ask(m.chat.id, "کپشن خود را ارسال نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
