7afba7456eefc562cf4a5c3849785898055b45da
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-06 21:32:32 +0430

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from os import getenv, environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Var(object):
+    API_ID = int(getenv('API_ID'))
+    API_HASH = str(getenv('API_HASH'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN'))
