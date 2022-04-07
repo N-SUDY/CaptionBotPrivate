787207dd51e37dd5ff43aab2575056c1b397e7d6
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-07 17:01:44 +0430

    Update __init__.py

--- Bot/plugins/__init__.py
+++ Bot/plugins/__init__.py
@@ -7,14 +7,14 @@ PASS = ["cipherx"]
 
 async def verifys(c: Client, m: Message):
     id = m.from_user.id
-    if id not in ADMINS:
+    if not id in ADMINS:
         verify = await c.ask(m.chat.id, "پسورد را وارد کنید")
-        if verify in PASS:
+        if verify.text in PASS:
             ADMINS.append(id) 
             await m.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون میتوانید از بات استفاده نمایید", quote=True) 
         if not verify.text:
             await verify.reply("پسوردی یافت نشد", quote=True)
             return await verifys(c, m)
-    if not verify in PASS:
+    if not verify.text in PASS:
         await verify.reply("پسورد اشتباه است و شما نمی‌توانید از بات استفاده نمایید", quote=True)
         return True
