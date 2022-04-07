a94ca5884d6fb10ff4707df3919597a06fe81204
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-07 21:02:42 +0430

    Update __init__.py

--- Bot/plugins/__init__.py
+++ Bot/plugins/__init__.py
@@ -4,14 +4,15 @@ from pyrogram.types import Message
 x = []
 PASS = ["cipherx"]
 
-
+@app.on_message(filters.incoming & filters.private & ~filters.edited)
 async def verifys(c: Client, m: Message):
     id = m.from_user.id
     if not id in x:
         verify = await c.ask(m.chat.id, "پسورد را وارد کنید")
         if verify.text in PASS:
             x.append(id) 
-            await m.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون میتوانید از بات استفاده نمایید", quote=True) 
+            await m.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون میتوانید از بات استفاده نمایید", quote=True)
+            return True 
         if not verify.text:
             await verify.reply("پسوردی یافت نشد", quote=True)
             return await verifys(c, m)
