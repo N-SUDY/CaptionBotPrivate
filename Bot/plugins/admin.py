f236d1682504d78b21d05cb430cf49b0befdbf8d
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-07 22:20:20 +0430

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -5,16 +5,23 @@ from Bot import app
 from Bot.plugins import x, PASS, verifys 
 
 
-@app.on_message(filters.user(*x) & filters.regex("پسورد") & filters.incoming & filters.private & ~filters.edited)
+@app.on_message(filters.incoming & filters.private & ~filters.edited)
 async def passphrase(c: Client, m: Message):
+    id = m.from_user.id
     cmd = m.text.split("_")[-1]
     if cmd.startswith("پسورد"):
-        p = await c.ask(m.chat.id, "پسورد مورد نظر خود را برای افزودن ادمین و دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-        PASS.append(p)
-        if not p.text:
-            await p.reply("پسوردی یافت نشد", quote=True) 
-        if p.text.startswith("کنسل"):
-            return True 
+        try:
+            if not id in x:
+                vf = await verifys(c, m) 
+            else:
+                p = await c.ask(m.chat.id, "پسورد مورد نظر خود را برای افزودن ادمین و دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
+                PASS.append(p)
+                if not p.text:
+                    await p.reply("پسوردی یافت نشد", quote=True) 
+                if p.text.startswith("کنسل"):
+                     return True 
+        except Exception as e:
+            return 
 
 @app.on_message(filters.incoming & filters.private & ~filters.edited)
 async def show_list(c: Client, m: Message):
@@ -25,6 +32,7 @@ async def show_list(c: Client, m: Message):
             if not id in x:
                 vf = await verifys(c, m) 
             else:
-                await m.reply(f"{x}", quote=True) 
+                for i in x:
+                    await m.reply(f"{i}", quote=True) 
         except Exception as e:
             return 
