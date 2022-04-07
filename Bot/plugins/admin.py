a53dcdd472051cfdd8e0b3bba9ede6233a0ad820
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-07 16:35:23 +0430

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from Bot import app
 from Bot.plugins import * 
 
-@app.on_message(filters.regex(x for str(x) in PASS) & filters.incoming & filters.private & ~filters.edited)
+@app.on_message(filters.incoming & filters.private & ~filters.edited)
 async def add_admin(c: Client, m: Message):
     id = m.from_user.id
     usr_cmd = m.text.split("_")[-1]
@@ -12,7 +12,7 @@ async def add_admin(c: Client, m: Message):
         ADMINS.append(id)
         await m.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون می توانید از بات استفاده نمایید") 
     else:
-        return
+        pass
 
 @app.on_message(filters.user(ADMINS) & filters.regex("پسورد") & filters.incoming & filters.private & ~filters.edited)
 async def passphrase(c: Client, m: Message):
