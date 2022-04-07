869d541d5812bfbb57847fb4194a1e82902fdcb0
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-07 14:53:44 +0430

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -5,23 +5,23 @@ from Bot.plugins import *
 
 @app.on_message(filters.incoming & filters.private & ~filters.edited)
 async def add_admin(c: Client, m : Message):
-    id = m.from_user.id
-    usr_cmd = m.text.split("_")[-1]
-    if usr_cmd == PASS:
-        ADMINS.append(id)
-        await m.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون می توانید از بات استفاده نمایید") 
-    else:
-        return 
+	id = m.from_user.id
+	usr_cmd = m.text.split("_")[-1]
+	if usr_cmd == PASS:
+		ADMINS.append(id)
+		await m.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون می توانید از بات استفاده نمایید") 
+	else:
+		return 
 
 @app.on_message(filters.user(ADMINS) & filters.regex("پسورد") & filters.incoming & filters.private & ~filters.edited) 
 async def passphrase(c: Client, m: Message):
-    usr_cmd = m.text.split("_")[-1]
-    if usr_cmd.startswith("پسورد"):
-        pass = await c.ask(m.chat.id, """پسورد مورد نظر خود را برای افزودن ادمین و دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`""")
-        PASS.append(pass)
-        if not pass.text:
-            await pass.reply("پسوردی یافت نشد", quote=True) 
-        if pass.text.startswith("کنسل"):
-            return True 
-        else:
-            return pass.text
+	usr_cmd = m.text.split("_")[-1]
+	if usr_cmd.startswith("پسورد"):
+		pass = await c.ask(m.chat.id, """پسورد مورد نظر خود را برای افزودن ادمین و دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`""")
+		PASS.append(pass)
+		if not pass.text:
+			await pass.reply("پسوردی یافت نشد", quote=True) 
+		if pass.text.startswith("کنسل"):
+			return True 
+		else:
+			return pass.text
