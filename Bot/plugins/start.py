443d926c53d11e0da6463433dd0647549d746313
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-07 23:03:56 +0430

    Update start.py

--- Bot/plugins/start.py
+++ Bot/plugins/start.py
@@ -4,19 +4,27 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from Bot import app
 from Bot.plugins import * 
 
-@app.on_message(filters.user(*x) & filters.command('start') & filters.incoming & filters.private & ~filters.edited)
+@app.on_message(filters.incoming & filters.private & ~filters.edited)
 async def start(b, m : Message):
     firstname = m.from_user.first_name
-    usr_cmd = m.text.split("_")[-1]
-    if usr_cmd == "/start":
-        await m.reply(
-            text=f"""سلام {m.from_user.mention(style="md")} عزیز 🙋🏻‍♂️\nمن بات ادیت کپشن هستم\nفایل تلگرامی خود را ارسال کنید تا کپشن آن را ادیت کنم""",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton('✵ Developer ✵', url='https://t.me/CipherXBot')]
-                ]
-            ),
-            disable_web_page_preview=True
-        )
-    else:
-        return 
+    id = m.from_user.id
+    cmd = m.text.split("_")[-1]
+    if cmd.startswith("/start"):
+        try:
+            if not id in x:
+                vf = await verifys(c, m) 
+            else:
+                await m.reply(
+                    text=f"""سلام {m.from_user.mention(style="md")} عزیز 🙋🏻‍♂️\nمن بات ادیت کپشن هستم\nفایل تلگرامی خود را ارسال کنید تا کپشن آن را ادیت کنم""",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [InlineKeyboardButton('✵ Developer ✵', url='https://t.me/CipherXBot')]
+                        ]
+                    ),
+                    disable_web_page_preview=True
+                ) 
+        except Exception as e:
+            return  
+
+
+
