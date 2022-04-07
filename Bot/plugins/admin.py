19d1f4987cfe659eb1da2d6aa002ccb5b24f8807
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-07 15:12:20 +0430

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -13,16 +13,3 @@ async def add_admin(c: Client, m : Message):
         await m.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون می توانید از بات استفاده نمایید") 
     else:
         return
-
-@app.on_message(filters.user(ADMINS) & filters.incoming & filters.private & ~filters.edited) 
-async def passphrase(c: Client, m: Message):
-    cmd = m.text.split("_")[-1]
-    if cmd.startswith("پسورد"):
-        pass = await c.ask(m.chat.id, "پسورد مورد نظر خود را برای افزودن ادمین و دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-        PASS.append(pass)
-        if not pass.text:
-            await pass.reply("پسوردی یافت نشد", quote=True) 
-        if pass.text.startswith("کنسل"):
-            return True 
-        else:
-            return pass.text
