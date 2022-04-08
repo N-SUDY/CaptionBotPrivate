3ae734b5a2999557832b2c7a3ded81e3d0f89a4a
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-08 18:01:50 +0430

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -104,13 +104,10 @@ async def add_admin(c: Client, m: Message):
                 if not p.text.startswith("کنسل") and p.text.isnumeric() is True:
                     x.append(int(p.text))
                     await p.reply("ادمین جدید با موفقیت افزوده شد", quote=True) 
-                    #return True
                 if p.text.isnumeric() is False and p.text != "کنسل":
                     await p.reply("لطفا فقط آیدی عددی وارد نمایید", quote=True) 
-                    #return True 
                 if not p.text and not p.text.startswith("کنسل"):
                     await p.reply("آیدی یافت نشد", quote=True) 
-                    #return True
                 if p.text.startswith("کنسل"):
                     await p.reply("فرایند کنسل شد", quote=True) 
                     return True 
@@ -131,15 +128,12 @@ async def rem_admin(c: Client, m: Message):
                 if not p.text.startswith("کنسل"):
                     if p.text.isnumeric() is True:
                         if not int(p.text) in x:
-                            await p.reply("آیدی در لیست ادمین ها یافت نشد", quote=True) 
-                            return True 
+                            await p.reply("آیدی در لیست ادمین ها یافت نشد", quote=True)  
                         else:
                             x.remove(int(p.text))
                             await p.reply("ادمین با موفقیت حذف شد", quote=True) 
-                            return True
                     if p.text.isnumeric() is False: 
                         await p.reply("لطفا فقط آیدی عددی وارد نمایید", quote=True) 
-                        return True
                 else:  
                     await p.reply("فرایند کنسل شد", quote=True) 
                     return True 
@@ -174,7 +168,6 @@ async def add_pass(c: Client, m: Message):
                 if not p.text.startswith("کنسل"):
                     PASS.append(p.text)
                     await p.reply("پسورد جدید با موفقیت افزوده شد", quote=True) 
-                    return True
                 if not p.text:
                     await p.reply("پسوردی یافت نشد", quote=True) 
                 else:
@@ -196,12 +189,10 @@ async def rem_pass(c: Client, m: Message):
                 p = await c.ask(m.chat.id, "پسورد مورد نظر خود را برای حذف و عدم دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
                 if not p.text.startswith("کنسل"):
                     if not p.text in PASS:
-                        await p.reply("پسورد در لیست پسورد ها یافت نشد", quote=True) 
-                        return True 
+                        await p.reply("پسورد در لیست پسورد ها یافت نشد", quote=True)  
                     else:
                         PASS.remove(p.text)
                         await p.reply("پسورد با موفقیت حذف شد", quote=True) 
-                        return True
                 if not p.text and not p.text.startswith("کنسل"):
                     await p.reply("پسورد یافت نشد", quote=True) 
                 if p.text.startswith("کنسل"):
