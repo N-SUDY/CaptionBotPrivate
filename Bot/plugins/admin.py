eafe68e5ebd84b86b434663dbb47723a68b2ff77
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-04-09 12:29:27 +0430

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -194,12 +194,12 @@ async def add_pass(c: Client, m: Message):
                 vf = await verifys(c, m) 
             else:
                 p = await c.ask(m.chat.id, "پسورد مورد نظر خود را برای افزودن ادمین و دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-                if p.text and not p.text.startswith("کنسل"):
+                if not p.text.startswith("کنسل"):
                     PASS.append(p.text)
                     await p.reply("پسورد جدید با موفقیت افزوده شد", quote=True) 
                 if not p.text:
                     await p.reply("پسوردی یافت نشد", quote=True) 
-                else:
+                if p.text.startswith("کنسل"): 
                     await p.reply("فرایند کنسل شد", quote=True) 
                     return True 
         except Exception as e:
