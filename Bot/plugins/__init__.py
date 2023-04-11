4d2f3fca87af3a488b509101c6980bf530d89377
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-04-11 19:19:53 +0330

    Update __init__.py

--- Bot/plugins/__init__.py
+++ Bot/plugins/__init__.py
@@ -1,7 +1,7 @@
 # All Credits Belong to @CipherXBot
 
 from pyrogram import Client, filters
-from pyrogram.types import Message 
+#from pyrogram.types import Message 
 
 x = []
 list1 = [] 
@@ -9,17 +9,17 @@ list2 = []
 PASS = ["ok"]
 
 
-async def verifys(c: Client, m: Message):
-    id = m.from_user.id
+async def verifys(c: Client, message):
+    id = message.from_user.id
     if not id in x:
-        verify = await c.ask(m.chat.id, "پسورد را وارد کنید")
+        verify = await c.ask(message.chat.id, "پسورد را وارد کنید")
         if verify.text in PASS:
             x.append(id) 
-            await m.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون میتوانید از بات استفاده نمایید", quote=True)
+            await message.reply("شما به لیست ادمین ها افزوده شدید و هم اکنون میتوانید از بات استفاده نمایید", quote=True)
             return id
         if not verify.text:
             await verify.reply("پسوردی یافت نشد", quote=True)
-            return await verifys(c, m)
+            return await verifys(c, message)
         if not verify.text in PASS:
             await verify.reply("پسورد اشتباه است و شما نمی‌توانید از بات استفاده نمایید", quote=True)
             return True
@@ -27,7 +27,7 @@ async def verifys(c: Client, m: Message):
         pass
 
 
-async def chad(c: Client, m: Message):
+async def chad(c: Client, message):
     if list1 and list2: 
         dic = dict(zip(list1, list2))
         return dic
