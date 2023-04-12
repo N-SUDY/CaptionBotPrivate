660a778545f65f3a5597cdd4cdcd4af51c4eec6c
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-04-12 21:21:03 +0330

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -7,7 +7,7 @@ from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid
 from Bot import app
 from Bot.plugins import *
 
-@app.on_message(filters.command("getid") & filters.channel)
+@app.on_message(filters.command("getid") & filters.incoming & filters.channel)
 async def id_channel(c: Client, m: Message):
     cmd = m.text.split("_")[-1]
     if cmd == "/getid":
