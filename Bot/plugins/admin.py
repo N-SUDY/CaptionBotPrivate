d0b760768afda1baa3506f1120c8f2aaf1d30e8c
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-04-12 20:57:51 +0330

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -7,11 +7,11 @@ from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid
 from Bot import app
 from Bot.plugins import *
 
-#@app.on_message(filters.command(["getid"]) & filters.channel)
+@app.on_message(filters.command(["getid"]) & filters.channel)
 async def id_channel(c: Client, m: Message):
     cmd = m.text.split("_")[-1]
     if cmd == "/getid":
-        await m.reply(f"آیدی : `-100{cmd.peer_id.channel_id}`")
+        await m.reply(f"آیدی : `-100{m.command.peer_id.channel_id}`")
             
 @app.on_message(filters.regex("آیدی") & filters.incoming & filters.private)
 async def id_command(c: Client, m: Message):
