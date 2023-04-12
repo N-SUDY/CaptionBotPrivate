2b5393b73cd4cc5247d733fa1d0bca8010d3954c
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-04-13 00:00:44 +0330

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,6 +1,10 @@
 # All Credits Belong to @CipherXBot
 
+import asyncio
+
 from pyrogram import Client
+from pyrogram.types import Message 
+
 from pyromod import listen
 
 from Bot.config import Var
@@ -13,3 +17,11 @@ app = Client(
     sleep_threshold=60,
     workers=4
 )
+
+async def pyromod_is_shit(c: Client, m: Message):
+    if m.from_user is None:
+        _id = m.sender_chat.id
+        return _id
+    else:
+        _id = m.from_user.id
+        return _id
