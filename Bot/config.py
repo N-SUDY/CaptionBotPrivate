0f721c42804deddd103e793adc0d5e24271ef914
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-06-19 19:16:40 +0330

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -10,4 +10,5 @@ class Var(object):
     API_ID = 6
     API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
     BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    OWNER_ID = str(getenv('OWNER_ID'))
     DEBUG = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
