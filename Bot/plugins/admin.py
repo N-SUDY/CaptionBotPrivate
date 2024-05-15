c30036cabcf4c8036eb57df3a9a798f2aaf6aedc
Unagi <115041575+N-SUDY@users.noreply.github.com>
2024-05-15 08:09:30 +0630

    Update admin.py

--- Bot/plugins/admin.py
+++ Bot/plugins/admin.py
@@ -1,4 +1,4 @@
-# All Credits Belong to @CipherXBot 
+# All Credits Belong to @CipherXBot
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, ChatMemberUpdated
@@ -26,11 +26,11 @@ async def track_admin_status(client: Client, chat_member_updated: ChatMemberUpda
                         first_name=admin.user.first_name or ""
                         last_name=admin.user.last_name or ""
                         channel_info_str=(
-                                f"#افزودن_بات\n"
-                                f"نام چنل : {chat.title}\n"
-                                f"آیدی چنل : {chat.id}\n"
-                                f"یوزرنیم مالک : @{owner_username}\n"
-                                f"مشخصات مالک :{first_name} {last_name}"
+                                f"#Bot_Addition\n"
+                                f"Channel Name: {chat.title}\n"
+                                f"Channel ID: {chat.id}\n"
+                                f"Owner Username: @{owner_username}\n"
+                                f"Owner Details: {first_name} {last_name}"
                             )
                         break
                     if channel_info_str:
@@ -39,10 +39,10 @@ async def track_admin_status(client: Client, chat_member_updated: ChatMemberUpda
                     print(f"Error: {str(e)}")
 
 
-@app.on_message(filters.regex("چتز") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Chats") & filters.incoming & filters.private)
 async def all_chats(c: Client, m: Message):
     cmd = m.text.split("_")[-1]
-    if cmd == "چتز":
+    if cmd == "Chats":
         if m.from_user.id == Var.OWNER_ID:
             try:
                 async for chat in app.get_dialogs():
@@ -58,250 +58,250 @@ async def all_chats(c: Client, m: Message):
 @app.on_message(filters.command(["id"]) & filters.channel)
 async def id_channel(c: Client, m: Message):
     try:
-        await m.reply(f"آیدی : `{m.chat.id}`")
+        await m.reply(f"ID: `{m.chat.id}`")
     except Exception as e:
         return str(e)
 
 
-@app.on_message(filters.regex("آیدی") & filters.incoming & filters.private)
+@app.on_message(filters.regex("ID") & filters.incoming & filters.private)
 async def id_command(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "آیدی":
+    if cmd == "ID":
         try:
-            x = await c.ask(m.chat.id, "یوزرنیم را بفرستید")
+            x = await c.ask(m.chat.id, "Send the username")
             chat = await c.get_chat(x.text)
             await c.send_message(
                 chat_id=m.chat.id,
-                text= f"آیدی {chat.title} : `{chat.id}`"
+                text= f"ID of {chat.title} : `{chat.id}`"
             )
         except Exception as er:
             await c.send_message(
                 chat_id=m.chat.id,
-                text= f"یوزرنیمی که فرستادید معتبر نمی باشد\nمتن ارور:\n{str(er)}"
+                text= f"The username you sent is not valid\nError message:\n{str(er)}"
             )
 
 
-@app.on_message(filters.regex("بفرس") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Send") & filters.incoming & filters.private)
 async def send(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "بفرس":
-        msg = await c.ask(m.chat.id, "بر روی فایل مورد نظر خود ریپلای کنید تا آن را برایتان به چنل داخواهتان در لیست چنل های اضافه شده بفرستم") 
+    if cmd == "Send":
+        msg = await c.ask(m.chat.id, "Reply to the desired file to send it to your channel list") 
         msg = msg.reply_to_message
         if msg:
-            chat = await c.ask(m.chat.id, "اکنون چت آیدی چنل مورد نظر خود را بفرستید. شما میتوانید چت آیدی چنل مورد نظر خود را از لیست چنل دریافت نمایید") 
+            chat = await c.ask(m.chat.id, "Now send the chat ID of your desired channel. You can get the chat ID of your desired channel from the channel list") 
             if chat.text.startswith("-100"):
                 await msg.copy(chat_id=int(chat.text))
-                await chat.reply(f"پست مورد نظر با موفقیت به چنل {(await app.get_chat(int(chat.text))).title} ارسال شد", quote=True)
+                await chat.reply(f"The post has been successfully sent to {(await app.get_chat(int(chat.text))).title} channel", quote=True)
             else:
-                await chat.reply("لطفا چت آیدی صحیح را وارد نمایید", quote=True) 
+                await chat.reply("Please enter the correct chat ID", quote=True) 
         else:
-            await c.send_message(m.chat.id, "شما بر روی فایل مورد نظر خود ریپلای نکردید")
+            await c.send_message(m.chat.id, "You did not reply to your desired file")
 
 
-@app.on_message(filters.regex("لیست چنل") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Channel List") & filters.incoming & filters.private)
 async def show_channels(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "لیست چنل":
+    if cmd == "Channel List":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
                 mad = await chad(c, m) 
                 if mad == None:
-                    await m.reply("لیست چنل خالی می باشد", quote=True) 
+                    await m.reply("The channel list is empty", quote=True) 
                 else:
                     await m.reply(f"{mad}", quote=True)  
         except Exception as e:
             return 
 
 
-@app.on_message(filters.regex("افزودن چنل") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Add Channel") & filters.incoming & filters.private)
 async def add_channel(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "افزودن چنل":
+    if cmd == "Add Channel":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
-                p = await c.ask(m.chat.id, "آیدی عددی چنل مورد نظر خود را وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-                if not p.text.startswith("کنسل") and p.text.startswith("-100"):
+                p = await c.ask(m.chat.id, "Enter the numerical ID of your desired channel. Write `cancel` to cancel this process")
+                if not p.text.startswith("cancel") and p.text.startswith("-100"):
                     try:
                         list1.append((await app.get_chat(int(p.text))).title)
                         list2.append(int(p.text))
                         await chad(c, m) 
-                        await p.reply("چنل جدید با موفقیت افزوده شد", quote=True) 
+                        await p.reply("New channel successfully added", quote=True) 
                     except ChannelInvalid:
-                        await p.reply("ابتدا بات را در چنل ادمین کرده و سپس چت آیدی آن را بفرستید", quote=True)
-                elif not p.text.startswith("-100") and p.text != "کنسل":
-                    await p.reply("لطفا فقط چت عددی چنل وارد نمایید", quote=True)  
-                elif not p.text and not p.text.startswith("کنسل"):
-                    await p.reply("چت آیدی یافت نشد", quote=True) 
-                elif p.text.startswith("کنسل"):
-                    await p.reply("فرایند کنسل شد", quote=True) 
+                        await p.reply("First add the bot as admin in the channel and then send the chat ID", quote=True)
+                elif not p.text.startswith("-100") and p.text != "cancel":
+                    await p.reply("Please enter only the numerical ID of the channel", quote=True)  
+                elif not p.text and not p.text.startswith("cancel"):
+                    await p.reply("Chat ID not found", quote=True) 
+                elif p.text.startswith("cancel"):
+                    await p.reply("Process canceled", quote=True) 
                     return True 
         except Exception as e:
             return 
 
     
-@app.on_message(filters.regex("حذف چنل") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Remove Channel") & filters.incoming & filters.private)
 async def rem_channel(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "حذف چنل":
+    if cmd == "Remove Channel":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
-                p = await c.ask(m.chat.id, "چت آیدی مورد نظر خود را برای حذف چنل وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-                if not p.text.startswith("کنسل"):
+                p = await c.ask(m.chat.id, "Enter the chat ID to remove the channel. Write `cancel` to cancel this process")
+                if not p.text.startswith("cancel"):
                     if p.text.startswith("-100"):
                         if not int(p.text) in list2:
-                            await p.reply("چت آیدی در لیست چنل ها یافت نشد", quote=True)  
+                            await p.reply("Chat ID not found in the channel list", quote=True)  
                         else:
-                            await p.reply("چنل با موفقیت حذف شد", quote=True)  
+                            await p.reply("Channel successfully removed", quote=True)  
                             await chad(c, m) 
                             list1.remove((await app.get_chat(int(p.text))).title)
                             list2.remove(int(p.text))
                             rm = dict.pop((await app.get_chat(int(p.text))).title)
                     elif not p.text.startswith("-100"): 
-                        await p.reply("لطفا فقط چت آیدی وارد نمایید", quote=True) 
+                        await p.reply("Please enter only the chat ID", quote=True) 
                 else:  
-                    await p.reply("فرایند کنسل شد", quote=True) 
+                    await p.reply("Process canceled", quote=True) 
                     return True 
         except Exception as e:
             return 
 
 
-@app.on_message(filters.regex("لیست ادمین") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Admin List") & filters.incoming & filters.private)
 async def show_admins(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "لیست ادمین":
+    if cmd == "Admin List":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
                 if x == []:
-                    await m.reply("لیست ادمین خالی می باشد", quote=True) 
+                    await m.reply("The admin list is empty", quote=True) 
                 else:
                     await m.reply(f"{x}", quote=True) 
         except Exception as e:
             return 
 
 
-@app.on_message(filters.regex("افزودن ادمین") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Add Admin") & filters.incoming & filters.private)
 async def add_admin(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "افزودن ادمین":
+    if cmd == "Add Admin":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
-                p = await c.ask(m.chat.id, "آیدی عددی مورد نظر خود را برای افزودن ادمین و دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-                if not p.text.startswith("کنسل") and p.text.isnumeric() is True:
+                p = await c.ask(m.chat.id, "Enter the numerical ID to add admin and access the bot. Write `cancel` to cancel this process")
+                if not p.text.startswith("cancel") and p.text.isnumeric() is True:
                     x.append(int(p.text))
-                    await p.reply("ادمین جدید با موفقیت افزوده شد", quote=True) 
-                elif p.text.isnumeric() is False and p.text != "کنسل":
-                    await p.reply("لطفا فقط آیدی عددی وارد نمایید", quote=True) 
-                elif not p.text and not p.text.startswith("کنسل"):
-                    await p.reply("آیدی یافت نشد", quote=True) 
-                elif p.text.startswith("کنسل"):
-                    await p.reply("فرایند کنسل شد", quote=True) 
+                    await p.reply("New admin added successfully", quote=True) 
+                elif p.text.isnumeric() is False and p.text != "cancel":
+                    await p.reply("Please enter only the numerical ID", quote=True) 
+                elif not p.text and not p.text.startswith("cancel"):
+                    await p.reply("ID not found", quote=True) 
+                elif p.text.startswith("cancel"):
+                    await p.reply("Process canceled", quote=True) 
                     return True 
         except Exception as e:
             return 
 
 
-@app.on_message(filters.regex("حذف ادمین") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Remove Admin") & filters.incoming & filters.private)
 async def rem_admin(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "حذف ادمین":
+    if cmd == "Remove Admin":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
-                p = await c.ask(m.chat.id, "آیدی عددی مورد نظر خود را برای حذف ادمین و عدم دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-                if not p.text.startswith("کنسل"):
+                p = await c.ask(m.chat.id, "Enter the numerical ID to remove admin and access to the bot. Write `cancel` to cancel this process")
+                if not p.text.startswith("cancel"):
                     if p.text.isnumeric() is True:
                         if not int(p.text) in x:
-                            await p.reply("آیدی در لیست ادمین ها یافت نشد", quote=True)  
+                            await p.reply("ID not found in the admin list", quote=True)  
                         else:
                             x.remove(int(p.text))
-                            await p.reply("ادمین با موفقیت حذف شد", quote=True) 
+                            await p.reply("Admin successfully removed", quote=True) 
                     elif p.text.isnumeric() is False: 
-                        await p.reply("لطفا فقط آیدی عددی وارد نمایید", quote=True) 
+                        await p.reply("Please enter only the numerical ID", quote=True) 
                 else:  
-                    await p.reply("فرایند کنسل شد", quote=True) 
+                    await p.reply("Process canceled", quote=True) 
                     return True 
         except Exception as e:
             return 
 
 
-@app.on_message(filters.regex("لیست پسورد") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Password List") & filters.incoming & filters.private)
 async def show_pass(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "لیست پسورد":
+    if cmd == "Password List":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
                 if PASS == []:
-                    await m.reply("لیست پسورد خالی می باشد", quote=True) 
+                    await m.reply("The password list is empty", quote=True) 
                 else:
                     await m.reply(f"{PASS}", quote=True) 
         except Exception as e:
             return 
 
 
-@app.on_message(filters.regex("افزودن پسورد") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Add Password") & filters.incoming & filters.private)
 async def add_pass(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "افزودن پسورد":
+    if cmd == "Add Password":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
-                p = await c.ask(m.chat.id, "پسورد مورد نظر خود را برای افزودن ادمین و دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-                if not p.text.startswith("کنسل"):
+                p = await c.ask(m.chat.id, "Enter the desired password to add admin and access the bot. Write `cancel` to cancel this process")
+                if not p.text.startswith("cancel"):
                     PASS.append(p.text)
-                    await p.reply("پسورد جدید با موفقیت افزوده شد", quote=True) 
+                    await p.reply("New password added successfully", quote=True) 
                 elif not p.text:
-                    await p.reply("پسوردی یافت نشد", quote=True) 
-                elif p.text.startswith("کنسل"): 
-                    await p.reply("فرایند کنسل شد", quote=True) 
+                    await p.reply("Password not found", quote=True) 
+                elif p.text.startswith("cancel"): 
+                    await p.reply("Process canceled", quote=True) 
                     return True 
         except Exception as e:
             return 
 
 
-@app.on_message(filters.regex("حذف پسورد") & filters.incoming & filters.private)
+@app.on_message(filters.regex("Remove Password") & filters.incoming & filters.private)
 async def rem_pass(c: Client, m: Message):
     id = m.from_user.id
     cmd = m.text.split("_")[-1]
-    if cmd == "حذف پسورد":
+    if cmd == "Remove Password":
         try:
             if not id in x:
                 vf = await verifys(c, m) 
             else:
-                p = await c.ask(m.chat.id, "پسورد مورد نظر خود را برای حذف و عدم دسترسی به بات وارد نمایید. برای کنسل کردن این فرایند بنویسید `کنسل`")
-                if not p.text.startswith("کنسل"):
+                p = await c.ask(m.chat.id, "Enter the desired password to remove admin and access to the bot. Write `cancel` to cancel this process")
+                if not p.text.startswith("cancel"):
                     if not p.text in PASS:
-                        await p.reply("پسورد در لیست پسورد ها یافت نشد", quote=True)  
+                        await p.reply("Password not found in the password list", quote=True)  
                     else:
                         PASS.remove(p.text)
-                        await p.reply("پسورد با موفقیت حذف شد", quote=True) 
-                elif not p.text and not p.text.startswith("کنسل"):
-                    await p.reply("پسورد یافت نشد", quote=True) 
-                elif p.text.startswith("کنسل"):
-                    await p.reply("فرایند کنسل شد", quote=True) 
+                        await p.reply("Password successfully removed", quote=True) 
+                elif not p.text and not p.text.startswith("cancel"):
+                    await p.reply("Password not found", quote=True) 
+                elif p.text.startswith("cancel"):
+                    await p.reply("Process canceled", quote=True) 
                     return True 
         except Exception as e:
-            return 
+            return
